f97e36899f842e97732175aa58c43f37f0bff114
ChristaTimil <christatimil@gmail.com>
2017-07-29 16:03:23 -0700

    added static root for heroku

--- ddapp/ddapp/settings.py
+++ ddapp/ddapp/settings.py
@@ -153,3 +153,15 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'home'
 SOCIAL_AUTH_URL_NAMESPACE = 'social' #Solved the twitter no 'social' module error
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
