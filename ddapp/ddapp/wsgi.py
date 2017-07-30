3d6de76e1df4ccc8b96e696b0049a6d86984cb73
ChristaTimil <christatimil@gmail.com>
2017-07-30 10:02:22 -0700

    added path append to wsgi

--- ddapp/ddapp/wsgi.py
+++ ddapp/ddapp/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ddapp.settings")
 
 application = get_wsgi_application()
+
+sys.path.append('/Users/christatimil/Projects/ddapp/ddapp')
